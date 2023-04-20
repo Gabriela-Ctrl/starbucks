a2e3f5a38c59b6ae4108643153d79e29177093ee
Gabriela-Ctrl <65233431+Gabriela-Ctrl@users.noreply.github.com>
2023-04-20 15:45:26 -0300

    Finalizado

--- mudar.js
+++ mudar.js
@@ -1,18 +1,19 @@
-// Fazer esta função 2º (Ela troca a imagem do sorvete grande)
+
+// 1ª Função (Ela altera o estado Ativo/Desativo)
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle'); // Cria uma variável e seleciona classe (.toggle)
+    var links =document.querySelector('.links'); // Cria uma variável e seleciona classe (.links)
+    menuToggle.classList.toggle('active') // Troca o estado da classe (.toggle) para ativo
+    links.classList.toggle('active') // Troca o estado da classe (.links) para ativo
+}
+
+// Troca a imagem (Do sorvete grande)
 function imgSlider(anything){
     document.querySelector('.starbucks').src = anything;
 }
 
-// Fazer esta função 3º (Ela troca a cor do circulo)
+// Troca a cor do círculo
 function changeCircleColor(color){
     const circulo =document.querySelector('.circulo');
-    circulo.style.background =color; 
-}
-
-// Fazer esta função 1º (Ela altera  o estado Ativado/Desativado)  
-function toggleMenu() {
-    var menuToggle = document.querySelector('.toggle');/*Cria uma variável e seleciona classe (.toggle)*/
-    var links =document.querySelector('.links');/*Cria uma variável e seleciona classe (.links)*/
-    menuToggle.classList.toggle('active')/*Troca o estado da, classe (.links) para ativo*/
-    links.classList.toggle('active')/*Troca o estado da, classe (.links) para */
+    circulo.style.background = color;
 }
